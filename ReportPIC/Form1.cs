aecf2441b424a8eae8869a624552e2da9fd68826
Daimom <dai_mom@hotmail.com>
2018-01-27 01:09:23 +0800

    加入專案檔案。

---
+++ ReportPIC/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportPIC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 欲連結外部圖片必須設定該屬性
+            reportViewer1.LocalReport.EnableExternalImages = true;
+            // 設定 Report 內的 @PhoroPath 參數資料
+            ReportParameter PhotoPath = new ReportParameter("par1", @"D:\aa.png");
+            reportViewer1.LocalReport.SetParameters(new ReportParameter[] { PhotoPath });
+            // 更新 ReportViewer            
+            this.reportViewer1.RefreshReport();
+
+
+
+    
+            var viewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            viewer.ProcessingMode = ProcessingMode.Local;
+            viewer.LocalReport.ReportPath = "Report1.rdlc";
+            viewer.LocalReport.EnableExternalImages = true;            
+            viewer.LocalReport.SetParameters(new ReportParameter[] { PhotoPath });
+            using (viewer)
+            {
+                byte[] bbytes;
+                bbytes = viewer.LocalReport.Render("Image", "<DeviceInfo><OutputFormat>PNG</OutputFormat></DeviceInfo>");
+                File.WriteAllBytes("d:\\test.png", bbytes);
+            }
+
+
+        }
+    }
+}
---
+++ ReportPIC/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。 
+// 變更這些屬性的值即可修改 
+// 組件的相關資訊。
+[assembly: AssemblyTitle("ReportPIC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportPIC")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的型別  
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中 
+// 的型別，請在該型別上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("5e88c104-d013-416d-a0f7-0fa122f47cc2")]
+
+// 組件的版本資訊由下列四個值所組成:
+//
+//      主要版本
+//      主要版本
+//      組建編號
+//      修訂
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
